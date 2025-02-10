a985bfd0091856e3c5fc25ede88c44c5a2dbf43a
BachTo <144211430+AnyaChann@users.noreply.github.com>
2025-02-10 15:58:20 +0700

    Login Servlet Update 2

--- WebAppDemo/src/main/java/controller/LoginServlet.java
+++ WebAppDemo/src/main/java/controller/LoginServlet.java
@@ -10,27 +10,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import model.User;
 
 @WebServlet(name = "LoginServlet", value = "/login")
-
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-        
-        //Step 1: Get the username and password from the request
+        // Step 1: Get the username and password from the request
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        //Step 2: Create an instance of the User class
+        // Step 2: Create an instance of the User class
         User user = new User();
         Boolean isValid = user.validation(username, password);
 
-        //Step 3: Điều hướng sang View
+        // Step 3: Forward to the appropriate view
         if (isValid) {
             req.setAttribute("username", username);
-            req.getRequestDispatcher("welcome.jsp").forward(req, resp);
+            req.getRequestDispatcher("/welcome.jsp").forward(req, resp);
         } else {
-            req.setAttribute("error", "Sai username hoặc password!");
-            req.getRequestDispatcher("login.jsp").forward(req, resp);
+            req.setAttribute("error", "Invalid username or password!");
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
         }
     }
 }
--- WebAppDemo/src/main/webapp/index.jsp
+++ WebAppDemo/src/main/webapp/index.jsp
@@ -1,16 +1,12 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Bach CT
-  Date: 2/10/2025
-  Time: 2:22 PM
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>LoginExamplePage</title>
 </head>
 <body>
-
+    <h2>Welcome to the Login Example Page</h2>
+    <form action="login.jsp" method="get">
+        <input type="submit" value="Go to Login Page">
+    </form>
 </body>
 </html>
